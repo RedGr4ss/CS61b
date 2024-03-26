9615716e972eb9f98c22cd54e7fdc7f11acc5613
RedGr4ss <RedGr4ss@163.com>
2024-03-26 21:38:32 +0800

    down with maxTileExists

--- proj0/game2048/Model.java
+++ proj0/game2048/Model.java
@@ -153,7 +153,15 @@ public class Model extends Observable {
      * given a Tile object t, we get its value with t.value().
      */
     public static boolean maxTileExists(Board b) {
-        // TODO: Fill in this function.
+        for(int i=0;i<4;i++){
+            for(int j=0;j<4;j++){
+                if(b.tile(i,j)==null){
+                    continue;
+                } else if (b.tile(i,j).value()==MAX_PIECE) {
+                    return true;
+                }
+            }
+        }
         return false;
     }
 
