1d047c6c9925a1525c4ddffd4f5b6c90fcaa0372
RedGr4ss <RedGr4ss@163.com>
2024-03-27 12:13:26 +0800

    done

--- proj0/game2048/Model.java
+++ proj0/game2048/Model.java
@@ -114,39 +114,37 @@ public class Model extends Observable {
         // for the tilt to the Side SIDE. If the board changed, set the
         // changed local variable to true.
         board.setViewingPerspective(side);
-        if(side==Side.NORTH) {
-            for (int c = 0; c < board.size(); c++) {
-                for (int r = board.size() - 1; r >= 0; r--) {
-                    Tile t1 = board.tile(c, r);
-                    if (t1 != null) {
-                        for (int r2 = r - 1; r2 >= 0; r2--) {
-                            Tile t2 = board.tile(c, r2);
-                            if (t2 != null) {
-                                if (t1.value() == t2.value()) {
-                                    board.move(c, r, t2);
-                                    changed = true;
-                                    r = r2;
-                                    score += 2 * t2.value();
-                                    break;
-                                }else{
-                                    break;
-                                }
+        for (int c = 0; c < board.size(); c++) {
+            for (int r = board.size() - 1; r >= 0; r--) {
+                Tile t1 = board.tile(c, r);
+                if (t1 != null) {
+                    for (int r2 = r - 1; r2 >= 0; r2--) {
+                        Tile t2 = board.tile(c, r2);
+                        if (t2 != null) {
+                            if (t1.value() == t2.value()) {
+                                board.move(c, r, t2);
+                                changed = true;
+                                r = r2;
+                                score += 2 * t2.value();
+                                break;
+                            } else {
+                                break;
                             }
                         }
                     }
                 }
             }
-            for (int c = 0; c < board.size(); c++) {
-                for (int r = board.size() - 1; r >= 0; r--) {
-                    Tile t1 = board.tile(c, r);
-                    if (t1 == null) {
-                        for (int r2 = r - 1; r2 >= 0; r2--) {
-                            Tile t2 = board.tile(c, r2);
-                            if (t2 != null) {
-                                    board.move(c, r, t2);
-                                    changed = true;
-                                    break;
-                            }
+        }
+        for (int c = 0; c < board.size(); c++) {
+            for (int r = board.size() - 1; r >= 0; r--) {
+                Tile t1 = board.tile(c, r);
+                if (t1 == null) {
+                    for (int r2 = r - 1; r2 >= 0; r2--) {
+                        Tile t2 = board.tile(c, r2);
+                        if (t2 != null) {
+                            board.move(c, r, t2);
+                            changed = true;
+                            break;
                         }
                     }
                 }
