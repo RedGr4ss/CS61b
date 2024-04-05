dcc45d5e8936bee9bd92b9a9e3453d8c9813ed1f
RedGr4ss <RedGr4ss@163.com>
2024-04-05 10:59:39 +0800

    整理代码规范

--- proj1/deque/MaxArrayDeque.java
+++ proj1/deque/MaxArrayDeque.java
@@ -3,11 +3,31 @@ package deque;
 import java.util.Comparator;
 
 public class MaxArrayDeque<T> extends ArrayDeque<T> {
-    Comparator<T> Ilist;
+    private final Comparator<T> Comp;
 
     public MaxArrayDeque(Comparator<T> t) {
         super();
-        Ilist = t;
+        Comp = t;
+    }
+
+    public static void main(String[] args) {
+        Comparator<Integer> Comp = new Comparator<>() {
+            @Override
+            public int compare(Integer o1, Integer o2) {
+                if (o1 > o2) {
+                    return 1;
+                } else if (o1 < o2) {
+                    return -1;
+                } else {
+                    return 0;
+                }
+            }
+        };
+        MaxArrayDeque<Integer> Alist = new MaxArrayDeque<>(Comp);
+        for (int i = 0; i < 500; i++) {
+            Alist.addFirst(i);
+        }
+        System.out.println(Alist.max());
     }
 
     public T max() {
@@ -16,7 +36,7 @@ public class MaxArrayDeque<T> extends ArrayDeque<T> {
         } else {
             T t = this.get(0);
             for (int i = 0; i < this.size; i++) {
-                if (Ilist.compare(t, this.get(i)) > 0) {
+                if (Comp.compare(this.get(i) , t) > 0) {
                     t = this.get(i);
                 }
             }
@@ -30,7 +50,7 @@ public class MaxArrayDeque<T> extends ArrayDeque<T> {
         } else {
             T t = this.get(0);
             for (int i = 0; i < this.size; i++) {
-                if (c.compare(t, this.get(i)) > 0) {
+                if (c.compare(this.get(i), t) > 0) {
                     t = this.get(i);
                 }
             }
