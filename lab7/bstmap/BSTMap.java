84f8e8dfb85decdb10d81b1ca80b372469a86520
RedGr4ss <RedGr4ss@163.com>
2024-05-07 15:36:44 +0800

    TestBSTMap passed

--- lab7/bstmap/BSTMap.java
+++ lab7/bstmap/BSTMap.java
@@ -1,6 +1,9 @@
 package bstmap;
 
+import java.security.Key;
+import java.util.HashSet;
 import java.util.Iterator;
+import java.util.NoSuchElementException;
 import java.util.Set;
 
 public class BSTMap <K extends Comparable,V> implements Map61B<K,V>{
@@ -14,6 +17,7 @@ public class BSTMap <K extends Comparable,V> implements Map61B<K,V>{
             Value=value;
             left=null;
             right=null;
+            size=1;
         }
     }
     private BSTnode root;
@@ -24,65 +28,88 @@ public class BSTMap <K extends Comparable,V> implements Map61B<K,V>{
 
     @Override
     public boolean containsKey(K key) {
-        if(get(key)!=null) {
-            return true;
-        }
-        return false;
+        return containsKey(root,key);
     }
-
+    private boolean containsKey(BSTnode node,K key) {
+        if(node==null){
+            return false;
+        }
+        int cmp= key.compareTo(node.Key);
+        if(cmp<0){
+            return containsKey(node.left,key);
+        }
+        if (cmp>0){
+            return containsKey(node.right,key);
+        }
+        return true;
+    }
     @Override
     public V get(K key) {
-        BSTnode node = root;
-        return get(node,key);
+        return getval(root,key);
     }
-    public V get(BSTnode node, K key){
+    public V getval(BSTnode node, K key){
         if(node==null){
             return null;
         }
         int cmp=key.compareTo(node.Key);
         if(cmp<0){
-            get(node.left,key);
+            return getval(node.left,key);
         }else if(cmp>0){
-            get(node.right,key);
+            return getval(node.right,key);
         }
         return node.Value;
     }
 
     @Override
     public int size() {
-        return root.size;
+        return size(root);
     }
-
+    private int size(BSTnode node){
+        if(node==null){
+            return 0;
+        }
+        return size(node.left)+size(node.right)+node.size;
+    }
     @Override
     public void put(K key, V value) {
-        if(containsKey(key)){
-            return;
-        }
-         put(root,key,value);
+        root=put(root,key,value);
     }
-    private void put(BSTnode node,K key, V value){
+    private BSTnode put(BSTnode node,K key, V value){
         if(node==null){
-            node.Key=key;
-            node.Value=value;
-            node.left=null;
-            node.right=null;
+             return new BSTnode(key, value);
         }
         int cmp = key.compareTo(node.Key);
         if(cmp<0){
-            put(node.left,key,value);
+            node.left = put(node.left,key,value);
+        }else if(cmp>0){
+            node.right = put(node.right,key,value);
         }else {
-            put(node.right,key,value);
+            node.Key=key;
+            node.Value=value;
         }
+        return node;
     }
 
     @Override
     public Set keySet() {
-        return null;
+        Set<K> set= new HashSet<>();
+        addset(root,set);
+        return set;
+    }
+    private void addset(BSTnode node,Set<K> set){
+        if(node != null){
+            addset(node.left,set);
+            set.add(node.Key);
+            addset(node.right,set);
+        }
     }
 
     @Override
     public V remove(K key) {
-        return null;
+        throw new UnsupportedOperationException("Remove operation is not supported.");
+    }
+    private V remove(BSTnode node ,K key){
+        throw new UnsupportedOperationException("Remove operation is not supported.");
     }
 
     @Override
